f91c5a6fd5a2b2e38b32207cd004604df8cafe35
Bozdi <arturbozdi@gmail.com>
2022-04-19 20:32:18 +0600

    19.04

--- app/src/main/java/com/bozdi/vapehubbeta/Profile.kt
+++ app/src/main/java/com/bozdi/vapehubbeta/Profile.kt
@@ -13,7 +13,7 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manager_profile, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
